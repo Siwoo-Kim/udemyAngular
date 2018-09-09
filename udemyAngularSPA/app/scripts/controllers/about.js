9e76f77f665425d33d40fb60f02170976d7068bf
HOMEPC <skim327@myseneca.ca>
2018-09-09 19:15:20 -0400

    first commit

---
+++ udemyAngularSPA/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udemyAngularSpaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the udemyAngularSpaApp
+ */
+angular.module('udemyAngularSpaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
